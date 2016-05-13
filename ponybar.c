ce73eb46527f2e5f1b999a7264056ed2f19249cf
Arch Pony <archpony@rambler.ru>
2016-05-13 18:21:37 +0300

    Code formatting

--- ponybar.c
+++ ponybar.c
@@ -42,6 +42,7 @@
 #define KBD_LEN		3
 #define IP_FORMAT      "IP:%s"
 #define IP_MISSED      "IP:down"
+#define ETH_NAME       "ens33"
 #define MAXTMPL        16
 #define MAXSTR         512
 #define MAXBUF         64
@@ -72,27 +73,27 @@ static long double cpu_first[4], cpu_last[4];
 static void XSetRoot(const char *name);
 
 int main(void){
-    char status[MAXSTR];
-	int ret = 0;     //placeholder for static part
-    char*off=status; //placeholder for some constant part
-	if(off>=(status+MAXSTR)){
-    	XSetRoot(status);
-        return 1;	/*This should not happen*/
-    }
+	char status[MAXSTR];
+	int  ret  = 0;       //placeholder for static part
+	char *off = status; //placeholder for some constant part
+	if (off>=(status + MAXSTR)) {
+		XSetRoot(status);
+		return 1;	/*This should not happen*/
+	}
 
 #ifdef USE_CPU
-    //cpu proc init
-    cpu_first[0] = -1;
+	//cpu proc init
+	cpu_first[0] = -1;
 #endif
     
 	char template[MAXTMPL];
 	snprintf(template, MAXTMPL, "%s%%s", SEPARATOR);
-    for(;;) {
+	for(;;) {
 		int   left=sizeof(status)-ret,i;
 		char* sta=off;
 		for(i = 0; i<sizeof(functab)/sizeof(functab[0]); ++i ) {
 			int ret;
-    		if (i!=0)
+			if (i!=0)
 				ret = snprintf(sta, left, template, functab[i]());
 			else
 				ret=snprintf(sta,left,"%s",functab[i]());
@@ -103,7 +104,7 @@ int main(void){
 		}
 		XSetRoot(status);
 		sleep(SLEEP_TIME);
-    }
+	}
 	return 0;
 }
 
@@ -126,25 +127,25 @@ static const char * date(void) { return ""; }
 #ifdef USE_RAM
 static const char * ram(void)
 {
-    long total, free, buffers, cached;
-    FILE *mf;
-    static char res[MAXBUF];
+	long total, free, buffers, cached;
+	FILE *mf;
+	static char res[MAXBUF];
 
-    if (!(mf= fopen("/proc/meminfo", "r"))) {
-        fprintf(stderr, "Error opening meminfo file.");
-        snprintf(res, MAXBUF, "n/a");
-        return res;
-    }
+	if (!(mf= fopen("/proc/meminfo", "r"))) {
+		fprintf(stderr, "Error opening meminfo file.");
+		snprintf(res, MAXBUF, "n/a");
+		return res;
+	}
 
-    fscanf(mf, "MemTotal: %ld kB\n", &total);
-    fscanf(mf, "MemFree: %ld kB\n", &free);
-    fscanf(mf, "MemAvailable: %ld kB\nBuffers: %ld kB\n", &buffers, &buffers);
-    fscanf(mf, "Cached: %ld kB\n", &cached);
+	fscanf(mf, "MemTotal: %ld kB\n", &total);
+	fscanf(mf, "MemFree: %ld kB\n",  &free);
+	fscanf(mf, "MemAvailable: %ld kB\nBuffers: %ld kB\n", &buffers, &buffers);
+	fscanf(mf, "Cached: %ld kB\n",   &cached);
 
-    fclose(mf);
+	fclose(mf);
 
-    snprintf(res, MAXBUF, RAM_FORMAT, ((total - free) - (buffers + cached))/1024);
-    return res;
+	snprintf(res, MAXBUF, RAM_FORMAT, ((total - free) - (buffers + cached))/1024);
+	return res;
 }
 #else
 static const char * ram(void) { return ""; }
@@ -154,19 +155,19 @@ static const char * ram(void) { return ""; }
 #ifdef USE_DISK
 static const char * disk(const char *mountpoint)
 {
-    static char vol[MAXBUF];
-    struct statvfs fs;
+	static char vol[MAXBUF];
+	struct statvfs fs;
 
-    if (statvfs(mountpoint, &fs) < 0) {
-        fprintf(stderr, "Could not get filesystem info.\n");
-        return "n/a";
-    }
+	if (statvfs(mountpoint, &fs) < 0) {
+		fprintf(stderr, "Could not get filesystem info.\n");
+		return "n/a";
+	}
 
-    unsigned long size = fs.f_bavail * fs.f_bsize;
+	unsigned long size = fs.f_bavail * fs.f_bsize;
 
-    snprintf(vol, MAXBUF, HDD_FORMAT, mountpoint, 1.0f*size/(1024*1024*1024));
+	snprintf(vol, MAXBUF, HDD_FORMAT, mountpoint, 1.0f*size/(1024*1024*1024));
 
-    return vol;
+	return vol;
 }
 #else
 static const char * disk(const char *s) { return ""; }
@@ -174,46 +175,48 @@ static const char * disk(const char *s) { return ""; }
 
 static const char * disk_root()
 {
-    return disk("/");
+	return disk("/");
 }
 
 static const char * disk_home()
 {
-    return disk("/home");
+	return disk("/home");
 }
 
 /* cpu percentage */
 #ifdef USE_CPU
 const char * cpu()
 {
-    int         perc;
-    static char cpu_usage[MAXBUF];
-    FILE        *fp;
+	int         perc;
+	static char cpu_usage[MAXBUF];
+	FILE        *fp;
 
-    if (!(fp = fopen("/proc/stat","r"))) {
-        fprintf(stderr, "Error opening stat file.");
-        return "n/a";
-    }
+	if (!(fp = fopen("/proc/stat", "r"))) {
+		fprintf(stderr, "Error opening stat file.");
+		return "n/a";
+	}
 
 	if (cpu_first[0]>0)
 		memcpy(cpu_first, cpu_last, 4*sizeof(long double));
     
 	fscanf(fp, "%*s %Lf %Lf %Lf %Lf", &cpu_last[0], &cpu_last[1], &cpu_last[2], &cpu_last[3]);
-
-    fclose(fp);
+	fclose(fp);
 
 	if (cpu_first[0]<0) {
 		memcpy(cpu_first, cpu_last, 4*sizeof(long double));
-    	snprintf(cpu_usage, MAXBUF, CPU_FORMAT, 0);
+		snprintf(cpu_usage, MAXBUF, CPU_FORMAT, 0);
 		return cpu_usage;
-     }
+	}
 
 
-    /* calculate avg */
-    perc = 100 * ((cpu_last[0]+cpu_last[1]+cpu_last[2]) - (cpu_first[0]+cpu_first[1]+cpu_first[2])) / ((cpu_last[0]+cpu_last[1]+cpu_last[2]+cpu_last[3]) - (cpu_first[0]+cpu_first[1]+cpu_first[2]+cpu_first[3]));
+	/* calculate avg */
+	perc = 100 * ((cpu_last[0]+cpu_last[1]+cpu_last[2]) - 
+		(cpu_first[0]+cpu_first[1]+cpu_first[2])) /
+		((cpu_last[0]+cpu_last[1]+cpu_last[2]+cpu_last[3]) - 
+		(cpu_first[0]+cpu_first[1]+cpu_first[2]+cpu_first[3]));
 
-    snprintf(cpu_usage, MAXBUF, CPU_FORMAT, perc);
-    return cpu_usage;
+	snprintf(cpu_usage, MAXBUF, CPU_FORMAT, perc);
+	return cpu_usage;
 }
 #else
 static const char * cpu(void){ return ""; }
@@ -226,19 +229,18 @@ static const char * cpu(void){ return ""; }
 
 Display* display;
 char	*group_names[XkbNumKbdGroups];
-int	group_num = 0;
+size_t	group_num = 0;
 
 static const char * kbd_lng(void)
 {
-
 	if (group_num==0) {
-    		int eventCode;
-		int errorReturn;
+		int event_code;
+		int err_code;
 		int major = XkbMajorVersion;
 		int minor = XkbMinorVersion;;
-		int reasonReturn;
-		display = XkbOpenDisplay( "", &eventCode, &errorReturn, &major, &minor, &reasonReturn);
-    		if (reasonReturn != XkbOD_Success)
+		int reason_code;
+		display = XkbOpenDisplay( "", &event_code, &err_code, &major, &minor, &reason_code);
+		if (reason_code != XkbOD_Success)
         		return "";
 		XkbDescRec* kbdDescPtr = XkbAllocKeyboard();
     		if (kbdDescPtr == NULL)
@@ -251,7 +253,7 @@ static const char * kbd_lng(void)
 			else break;
 		XGetAtomNames(display, grpAtoms, group_num, group_names);
 		XkbFreeNames(kbdDescPtr, XkbGroupNamesMask, True);
-		for (int i=0;i<group_num&&KBD_LEN>0; ++i)
+		for (size_t i=0;i<group_num&&KBD_LEN>0; ++i)
 			group_names[i][KBD_LEN]='\0';
 	}
 
@@ -271,24 +273,24 @@ static const char * kbd_lng(void)
 #ifdef USE_IP
 #include <netdb.h>
 #include <ifaddrs.h>
-static const char *ip_addr(char *iface)
+static const char * ip_addr(char *iface)
 {
 	struct ifaddrs *ifaddr;
 	int  s;
 	static char host[NI_MAXHOST];
 	static char ip[MAXBUF];
 
-	if (getifaddrs(&ifaddr) == -1) {
+	if (getifaddrs(&ifaddr) == -1) 
 		return "n/a";
-	}
-	strncpy(ip, IP_MISSED, MAXBUF);
+
+	strncpy(ip, IP_MISSED, MAXBUF-1);
 
 	for(struct ifaddrs *ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
 		if((ifa->ifa_addr->sa_family!=AF_INET)) continue;
 		if (ifa->ifa_addr == NULL) continue;
 		s = getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in), host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
 		if (s != 0) {
-			strncpy(ip, gai_strerror(s), MAXBUF);
+			strncpy(ip, gai_strerror(s), MAXBUF-1);
 			break;
 		}
 		if (strcmp(iface,ifa->ifa_name)==0) {
@@ -300,9 +302,9 @@ static const char *ip_addr(char *iface)
 	return ip;
 }
 
-static const char *ip_eth(void)
+static const char * ip_eth(void)
 {
-	return ip_addr("ens33");
+	return ip_addr(ETH_NAME);
 }
 #else
 static const char * ip_eth(void)
